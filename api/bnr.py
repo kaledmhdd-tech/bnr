42ae566baf82f846d454905a93da361eb9ddf317
kaledmhdd-tech <kaledmhdd@gmail.com>
2026-01-01 21:58:31 +0100

    Update API URL to new endpoint for account info

--- api/bnr.py
+++ api/bnr.py
@@ -131,7 +131,7 @@ def generate_avatar_only():
 
     try:
         # ✅ API الجديد
-        api_url = f"https://info-lf8r.onrender.com/api/account?uid={uid}&region=ME"
+        api_url = f"https://info-eight-rho.vercel.app/accinfo?uid={uid}&region=IND"
         res = requests.get(api_url, timeout=40)
         res.raise_for_status()
         data = res.json()
